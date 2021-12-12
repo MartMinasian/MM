4f2c8ad19915dfec5ae61d9ab6f8e50bfc616f26
MartMinasian <m3057433@yandex.ru>
2021-12-12 23:42:28 +0300

    MM

--- api/plus.php
+++ api/plus.php
@@ -1,9 +1,10 @@
 <?php
-
+session_start();
 // Это комментарий
 $x = $_REQUEST["x"];
 $y = $_REQUEST["y"];
 $z = $x + $y;
+$user = $_SESSION["user"];
 
 // Здесь нарушены принципы безопасности:
 // 1. Принцип наименьших привелегий
@@ -13,15 +14,15 @@ $z = $x + $y;
 //$conn = mysqli_connect("localhost","root","","calc");
 // 4. Код уязвимый для Sql-injection
 
-include ("\\Users\\martun\\AppParams\\params.php");
-//у меня мак и ссылки на диск С нет. Ошибка в написании команды!
+include("/Applications/XAMPP/xamppfiles/htdocs/MM/AppParams/params.php");
+//у меня мак и ссылки на диск С нет. 
 
 $conn = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
 
-$sql = "INSERT INTO calc(Number1,Number2,Result,UserID) VALUES(11,22,33,'anonym')";
+$sql = "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,'$user')";
 mysqli_query($conn,$sql);
-//echo (mysqli_error($conn));
+echo (mysqli_error($conn));
 mysqli_close($conn);
 
 echo($z);
-
+?>
--- params.php
+++
@@ -1,5 +0,0 @@
-<?php
-$DB_URL = "localhost";
-$DB_USER = "root";
-$DB_PWD = "";
-$DB_NAME = "calc";
