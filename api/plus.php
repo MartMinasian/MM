0bbfd83c3e07fc4b48d718f99369787fcb05d8df
MartMinasian <m3057433@yandex.ru>
2021-12-14 01:05:37 +0300

    MM

--- api/plus.php
+++ api/plus.php
@@ -4,6 +4,7 @@
 $x = $_REQUEST["x"];
 $y = $_REQUEST["y"];
 $z = $x + $y;
+$user = $_SESSION["user"];
 
 // Здесь нарушены принципы безопасности:
 // 1. Принцип наименьших привелегий
@@ -13,12 +14,12 @@ $z = $x + $y;
 //$conn = mysqli_connect("localhost","root","","calc");
 // 4. Код уязвимый для Sql-injection
 
-include (getenv("MYAPP_CONFIG"]));
+include (getenv("MYAPP_CONFIG"));
 //у меня мак и ссылки на диск С нет. Ошибка в написании команды!
 
 $conn = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
-
-$sql = "INSERT INTO calc(Number1,Number2,Result,UserID) VALUES(11,22,33,'anonym')";
+$sql = "INSERT INTO log(NUMBER1,NUMBER2,Result,UserID) VALUES($x,$y,$z,'$user')";
+//$sql = "INSERT INTO calc(Number1,Number2,Result,UserID) VALUES(11,22,33,'anonym')";
 mysqli_query($conn,$sql);
 //echo (mysqli_error($conn));
 mysqli_close($conn);
