07c673a7f30ecfa4df07618aa1dd3a15728e2d9e
MartMinasian <m3057433@yandex.ru>
2021-12-14 20:05:06 +0300

    mm

---
+++ api/get_log.php
@@ -0,0 +1,43 @@
+<?php
+    session_start();
+// Если жетона безопасности (т.е., в нашем случае,
+// сессионной переменной с названием user)нет, "не пущаем"
+   if (!isset($_SESSION["user"])) {
+    echo('<meta http-equiv="refresh" content="2; URL=../login.php">');
+    die("Требуется логин!");
+   } 
+   $user = $_SESSION["user"];
+    include(getenv("MYAPP_CONFIG"));
+    //include("/Applications/XAMPP/xamppfiles/htdocs/MM/AppParams/params.php");
+?>
+   <html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <?php
+           //Оставим уязвимость sql-injection для спортивных управжнений
+            $sql = "SELECT ID, NUMBER1, Number2,Result, UserID
+                    FROM log 
+                    WHERE UserID='$user'
+            ";
+
+            $conn = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
+            //echo $sql;
+            //Нудная, но необходимая процедура передачи параметов 
+            //в sql выражение, что гарантирует защиту от инжекции sql
+
+            $statement = mysqli_prepare($conn, $sql);
+            mysqli_stmt_execute($statement);
+            echo(mysqli_error($conn));
+            $cursor = mysqli_stmt_get_result($statement);
+            $result = mysqli_fetch_all($cursor);
+
+            echo(mysqli_error($conn));
+            mysqli_close($conn);
+
+            //var_dump($result);
+            echo json_encode($result);
+        ?>
+    </body>
+</html>
