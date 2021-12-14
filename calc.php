6e612b3adae50f00058148b5d6890576ade9c6ab
MartMinasian <m3057433@yandex.ru>
2021-12-14 22:23:27 +0300

    MM

--- calc.php
+++ calc.php
@@ -4,13 +4,7 @@ session_start();
 if (!isset($_SESSION["user"])) {
     echo('<meta http-equiv="refresh" content="2; URL=login.php">');
 }
-
-
-
 ?>
-
-
-
 <html>
         <head>
  
@@ -32,13 +26,22 @@ if (!isset($_SESSION["user"])) {
             </style>
             <script>
                 function plus() {
-                 var x = document.getElementById("x").value;
-                  var y =  document.getElementById("y").value; 
-                  var z = parseInt(x) + parseInt(y);
-                  document.getElementById ("z").value = z;
-                  document.getElementById ("btn1").className = "pressed";
-                  document.getElementById ("btn2").className = "";
-                }
+                //Это комментарий JS
+                var x = document.getElementById("x").value;
+                var y = document.getElementById("y").value;
+                
+                var url = "api/plus.php?x=" + x + "&y=" + y;
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET",url,false);
+                xhr.send();
+                var z = xhr.responseText;
+
+               // <br /><b>Notice</b>:  Undefined variable: _SESSION in <b>C:\xampp\htdocs\MM\api\plus.php</b> on line <b>7</b><br />сфы
+
+                document.getElementById("z").value = z;
+                document.getElementById("btn1").className = "pressed";
+                document.getElementById("btn2").className = "";
+            }
                   function minus() {
                  var x = document.getElementById("x").value;
                   var y =  document.getElementById("y").value; 
